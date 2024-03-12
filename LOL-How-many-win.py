a3af8537c909d298dd7952aeff6238366b44cf1f
Jeongminseo <20181482@sch.ac.kr>
2024-03-12 22:20:06 +0900

    3m12d

--- LOL-How-many-win.py
+++ LOL-How-many-win.py
@@ -1,13 +1,42 @@
 # Load packages and dataset
-import inline
-import matplotlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#%matplotlib inline 해당 코드는 jupyter에서 작동되는 코드
+# %matplotlib inline 해당 코드는 jupyter에서 작동되는 코드
 sns.set_style('darkgrid')
 
 df = pd.read_csv('high_diamond_ranked_10min.csv')
 df.head()
+
+# EDA
+# check missing values and data type
+df.info()  # int or float
+
+df_clean = df.copy()
+
+# Drop some unecessary columns. e.g. blueFirstblood/redfirst blood blueEliteMonster/redEliteMonster blueDeath/redKills etc are repeated
+# 불필요한 변수 삭제
+# Based on personal experience with the game, mimion yield gold+experience, we can drop minion kill too
+# 미니언 수율 골드+경험치를 바탕으로 미니언 킬도 드롭할 수 있습니다.
+
+cols = ['gameId', 'redFirstBlood', 'redKills', 'redEliteMonsters', 'redDragons', 'redTotalMinionsKilled',
+        'redTotalJungleMinionsKilled', 'redGoldDiff', 'redExperienceDiff', 'redCSPerMin', 'redGoldPerMin', 'redHeralds',
+        'blueGoldDiff', 'blueExperienceDiff', 'blueCSPerMin', 'blueGoldPerMin', 'blueTotalMinionsKilled']
+df_clean = df_clean.drop(cols, axis=1)
+
+df_clean.info()
+
+# Next let's check the relationship between parameters of blue team features
+# 블루팀의 매개변수를 확인
+g = sns.PairGrid(data=df_clean, vars=["blueKills", "blueAssists", "blueWardsPlaced", "blueTotalGold"], hue='blueWins',
+                 palette='Set1')
+g.map_diag(plt.hist)
+g.map_offdiag(plt.scatter)
+g.add_legend()
+plt.show()
+
+# 많은 공선성을 보인다.
+# 공선성(collinearity): 하나의 독립변수가 다른 하나의 독립변수로 잘 예측되는 경우, 또는 서로 상관이 높은 경우
+
